e8602f6426f0beee9a94f210fbaa2e3df9dd1d9f
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-02 23:19:19 +0530

    Added a function to get the product by product ID

--- controllers/controler.js
+++ controllers/controler.js
@@ -1,10 +1,23 @@
 const Product = require("../models/productModel")
 
+
+//fetch all products
 const getAllProducts = (req, res) => {
 
     Product.find()
     .then(response => res.json({response}))
     .catch(error => res.json({error}))
 }
+//fetch product by id
+//assuming id is passed in the request body
+const getProductById = (req, res) => {
+
+    const id = req.body.id
+
+    Product.findOne({id: id})
+    .then(response => res.json({response}))
+    .catch(error => res.json({error}))
+}
 
 exports.getAllProducts = getAllProducts
+exports.getProductById = getProductById
