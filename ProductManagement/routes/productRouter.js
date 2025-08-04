36712b14dfb27161948dccc899828dbc84ad23c6
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-04 23:06:40 +0530

    Removed an unnecessary route

--- ProductManagement/routes/productRouter.js
+++ ProductManagement/routes/productRouter.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const controler = require('../controllers/controler')
 
 
-router.post('/verifylogin', controler.verifyToken)
 
 
 /////////////////////////////////////////////
