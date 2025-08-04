a2e3c30b20284dcd6e074fced15f77712735fcf0
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-04 23:22:13 +0530

    Added two routes(getAllProducts and getProductById)

--- ProductManagement/routes/productRouter.js
+++ ProductManagement/routes/productRouter.js
@@ -9,6 +9,8 @@ const controler = require('../controllers/controler')
 
 
 
+router.post('/getallproducts', controler.getAllProducts)
+router.post('/getproductbyid', controler.getProductById)
 router.post('/addnewproduct', controler.addNewProduct)
 router.post('/updateproduct', controler.updateProduct)
 
