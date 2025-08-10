33f84f59657dabb306cd81d7acda16ab5f4e6b02
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-10 23:25:39 +0530

    Added three routs(update inventory item, delete inventory item, get inventory by cloth id

--- inventoryStockSupplierManagement/routes/inventoryRoute.js
+++ inventoryStockSupplierManagement/routes/inventoryRoute.js
@@ -48,3 +48,36 @@ router.post('/add', async (req, res) => {
 
 
 });
+
+// Update an inventory item
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedInventoryItem = await Inventory.findOneAndUpdate({_id: req.params.id}, req.body, { new: true });
+        res.json({updatedInventoryItem});
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete an inventory item
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedInventoryItem = await Inventory.findByIdAndDelete(req.params.id);
+        if (!deletedInventoryItem) {
+            return res.status(404).json({ message: 'Inventory item not found' });
+        }
+        res.json({ message: 'Inventory item deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get('/:clothId', async (req, res) => {
+    try {
+        const clothId = req.params.clothId;
+        const inventoryItem = await Inventory.findOne({ _id: clothId });
+        res.json({inventoryItem});
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
