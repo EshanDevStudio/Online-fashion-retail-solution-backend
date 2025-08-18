3813cb7ce5487165c4f51f411aee20bcf1cb4a5e
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-18 23:23:12 +0530

    Added a route to retrieve all stock items

--- inventoryStockSupplierManagement/routes/stockRoute.js
+++ inventoryStockSupplierManagement/routes/stockRoute.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Stock = require('../models/stockModel');
+
+// Retrieve all stock items
+router.get('/', async (req, res) => {
+    try {
+        const stock = await Stock.find();
+        res.status(200).json(stock);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
