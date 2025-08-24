21f2aebdbf3928ff4ee9c96103199ad8cc132cea
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-24 23:23:37 +0530

    Added a route to retrieve all suppliers

--- inventoryStockSupplierManagement/routes/supplierRoute.js
+++ inventoryStockSupplierManagement/routes/supplierRoute.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Supplier = require('../models/supplierModel');
 
-
+// Retrieve all suppliers
+router.get('/', async (req, res) => {
+    try {
+        const suppliers = await Supplier.find();
+        res.status(200).json(suppliers);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
