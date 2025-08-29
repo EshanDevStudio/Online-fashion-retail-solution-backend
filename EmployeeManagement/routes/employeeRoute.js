a6743fd250707c6bac5b6ec525fc0ab98feba3c0
EshanDevStudio <eshan.devstudio@gmail.com>
2025-08-29 23:56:14 +0530

    Added a new route to create a new employee

--- EmployeeManagement/routes/employeeRoute.js
+++ EmployeeManagement/routes/employeeRoute.js
@@ -12,4 +12,27 @@ router.get('/',  async (req, res) => {
   }
 });
 
+// Route to create a new employee
+router.post('/', async (req, res) => {
+  const employee = new Employee({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    salary: req.body.salary,
+    otrate: req.body.otrate,
+    totalot: req.body.totalot,
+    totalsalary: req.body.totalsalary,
+    password:  req.body.password,
+    role:  req.body.role
+  });
+
+  try {
+    const newEmployee = await employee.save();
+    res.status(201).json(newEmployee);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 module.exports = router;
